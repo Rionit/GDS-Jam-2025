51768b8c2e848fa04551532497fc216e2aa12c8c
Filip Doležal <fidokadan@gmail.com>
2025-11-30 12:10:41 +0100

    Added audiomanager

---
+++ scripts/AudioManager.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+@onready var sfx: AudioStreamPlayer = $SFX
+@onready var music: AudioStreamPlayer = $Music
+
+func _ready() -> void:
+	pass
+
+func play_sfx(stream: AudioStream, volume: float = 1.0) -> void:
+	sfx.stream = stream
+	sfx.volume_db = volume
+	print("playing sound " + stream.resource_path)
+	sfx.play()
+
+func play_music(stream: AudioStream, volume: float = -80.0) -> void:
+	music.stream = stream
+	music.volume_db = volume
+	print("playing msuic " + stream.resource_path)
+	music.play()
+
+func stop_music() -> void:
+	music.stop()
+
+func fade_in_music(duration: float = 1.0):
+	fade_music(0.0, Tween.EASE_IN, duration)
+	
+func fade_out_music(duration: float = 1.0):
+	fade_music(-80.0, Tween.EASE_OUT, duration)
+
+func fade_music(target_db: float, fade_ease: Tween.EaseType = Tween.EASE_IN_OUT, duration: float = 1.0) -> void:
+	var tween := get_tree().create_tween()
+	tween.tween_property(music, "volume_db", target_db, duration)
+	await tween.finished
+
+func is_music_playing() -> bool:
+	return music.playing
+
+func is_sfx_playing() -> bool:
+	return sfx.playing
