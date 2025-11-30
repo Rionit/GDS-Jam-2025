c4b5910b387426f4ad6a1c1dd657565f9066e08e
Arina Streltsova <arinastreltsova@Arinas-MacBook-Air.local>
2025-11-30 12:36:53 +0100

    added ui turkish sounds

--- scripts/TurkishGuy.gd
+++ scripts/TurkishGuy.gd
@@ -6,7 +6,13 @@ extends Control
 @onready var exit: TurkItem = $Him/Exit
 @onready var hair: TurkItem = $Him/Hair
 
+const POPUP_UI_MUSIC = preload("uid://b37mocg28w6xw")
+
 func _ready() -> void:
+	AudioManager.fade_out_music()
+	AudioManager.play_music(POPUP_UI_MUSIC)
+	AudioManager.fade_in_music()
+	
 	scissors.modulate.a = 0.0
 	exit.modulate.a = 0.0
 	hair.modulate.a = 0.0
@@ -18,3 +24,7 @@ func _ready() -> void:
 	tween.tween_property(scissors, "modulate:a", 1.0, 0.5) 
 	tween.tween_property(exit, "modulate:a", 1.0, 0.5) 
 	tween.tween_property(hair, "modulate:a", 1.0, 0.5) 
+
+func bald_sfx():
+	AudioManager.play_sfx(load("res://sound/turkish_lvl/popup_ui/THE_BALD.wav"), -5.0)
+	pass
